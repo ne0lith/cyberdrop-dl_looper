bbcfc010d499009adae372b515babf8ae921ec85
n30liberal <118079899+n30liberal@users.noreply.github.com>
2023-12-06 17:36:48 -0600

    Update file_management.py

--- mods/file_management.py
+++ mods/file_management.py
@@ -6,29 +6,26 @@ from pathlib import Path
 
 
 def backup_logs(profile_name):
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%I_%M_%S_%p")
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d__%I_%M_%S_%p")
 
     logs_dir = "Logs"
     archived_logs_dir = "Archived_Logs"
-    log_dir = f"{timestamp}"
 
     current_dir = Path(__file__).resolve().parent
     profile_dir = current_dir.parent / "AppData" / "Configs" / profile_name
-    logs_path = profile_dir / logs_dir
-    archived_logs_path = profile_dir / archived_logs_dir
-    urls_file = profile_dir / "URLS.txt"
 
-    # Create "Archived_Logs" directory if it doesn't exist
+    logs_path = profile_dir / logs_dir
+
+    archived_logs_path = profile_dir / archived_logs_dir
     archived_logs_path.mkdir(parents=True, exist_ok=True)
 
-    # Create a copy of "Logs" to "Archived_Logs"
-    shutil.copytree(logs_path, archived_logs_path / logs_dir)
+    backup_logs_path = archived_logs_path / timestamp
+    shutil.copytree(logs_path, backup_logs_path)
 
-    # Rename the copied "Logs" to the generated timestamp
-    (archived_logs_path / logs_dir).rename(archived_logs_path / log_dir)
+    urls_file = profile_dir / "URLS.txt"
+    shutil.copy(urls_file, backup_logs_path / "URLS.txt")
 
-    # Copy the URLs file to the archived logs directory
-    shutil.copy(urls_file, archived_logs_path / log_dir / "URLS.txt")
+    print(f"Logs backed up successfully to {backup_logs_path}")
 
 
 def main():
