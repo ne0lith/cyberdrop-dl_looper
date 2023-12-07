249c08e74bc82eb29d0eb7f9075057b953892585
n30liberal <118079899+n30liberal@users.noreply.github.com>
2023-12-07 15:40:30 -0600

    Update looper_mods.sh

--- looper_mods.sh
+++ looper_mods.sh
@@ -2,13 +2,20 @@
 
 NUM_RUNS=20
 PROFILE="Default"
+BACKUP_LOGS=false  # Set to true to enable file_management.py
+SEND_WEBHOOK=false  # Set to true to enable send_webhook_results.py
 
 for ((i = 1; i <= NUM_RUNS; i++)); do
     echo "Running start.sh - Iteration $i"
     sh start.sh
 
-    python3 mods/file_management.py "$PROFILE"
-    python3 mods/send_webhook_results.py "$PROFILE"
+    if [ "$BACKUP_LOGS" = true ]; then
+        python3 mods/file_management.py "$PROFILE"
+    fi
+
+    if [ "$SEND_WEBHOOK" = true ]; then
+        python3 mods/send_webhook_results.py "$PROFILE"
+    fi
 
     echo "Completed iteration $i"
     echo
