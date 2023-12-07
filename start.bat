5222b7580834581351d9759107b80a7bf6da1c4a
n30liberal <118079899+n30liberal@users.noreply.github.com>
2023-12-07 00:46:55 -0600

    Update start.bat

--- start.bat
+++ start.bat
@@ -11,7 +11,7 @@ if not defined PYTHON (set PYTHON=python)
 if not defined VENV_DIR (set "VENV_DIR=%~dp0%venv")
 
 If Not Exist "%VENV_DIR%\Scripts\activate.bat" (
-    for /f %%i in ('CALL %PYTHON% -c "import sys; print(sys.executable)"') do set FULL_PATH="%%i"
+    for /f %%i in ('CALL %PYTHON% -c "import sys; print(sys.executable)"') do set "FULL_PATH=%%i"
 
     echo Creating virtual environment
     !FULL_PATH! -m venv "%VENV_DIR%"
