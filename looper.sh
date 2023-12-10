4be1975d8021132589d41bf3b96f46ee34bbf71e
ne0lith <118079899+ne0lith@users.noreply.github.com>
2023-12-10 15:22:03 -0600

    Update looper.sh

--- looper.sh
+++ looper.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-source config.txt
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+. "$SCRIPT_DIR/config.txt"
 
-for ((i = 1; i <= loop_count; i++)); do
-    echo "Running start.sh - Iteration $i"
+for i in $(seq 1 $loop_count); do
+    echo "Running start.sh - Iteration $i of $loop_count"
     sh start.sh
-    echo "Completed iteration $i"
+    echo "Completed iteration $i of $loop_count"
     echo
 done
