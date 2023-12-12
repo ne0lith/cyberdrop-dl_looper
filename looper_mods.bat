203b8423ec26c33c892fe205a108c9c581435783
ne0lith <118079899+ne0lith@users.noreply.github.com>
2023-12-12 15:17:15 -0600

    Update looper_mods.bat
    Add sleep to the end of a loop

--- looper_mods.bat
+++ looper_mods.bat
@@ -11,16 +11,19 @@ for /L %%i in (1, 1, %loop_count%) do (
     call :RunSubBatch
 
     REM Call Postrun functions
-
     REM Backup all logs each run (this can take up GIGS in no time. be sure to delete every once in a while.)
     if "%backup_logs%"=="true" (
         python "mods\file_management.py" "!profile_name!"
     )
-
     REM Send a discord webhook to notify you of final numbers
     if "%send_webhook%"=="true" (
         python "mods\send_webhook_results.py" "!profile_name!" "!machine_name!"
     )
+
+    echo Completed iteration %%i of %loop_count%
+    echo
+    echo Sleeping for !sleep_time! seconds before the next iteration...
+    timeout /t !sleep_time! /nobreak
 )
 
 goto :eof
