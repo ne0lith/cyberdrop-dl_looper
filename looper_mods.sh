cd6b102d86fc9d1c85232b454dc55892a5924061
ne0lith <118079899+ne0lith@users.noreply.github.com>
2023-12-12 15:12:50 -0600

    Update looper_mods.sh
    Add sleep to each loop iteration

--- looper_mods.sh
+++ looper_mods.sh
@@ -18,4 +18,6 @@ for i in $(seq 1 $loop_count); do
 
     echo "Completed iteration $i of $loop_count"
     echo
+    echo "Sleeping for $sleep_time seconds before the next iteration..."
+    sleep "$sleep_time"
 done
