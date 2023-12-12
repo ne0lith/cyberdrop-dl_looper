e4f724a46fa50513a65f2b78e5bde8d649a98f93
ne0lith <118079899+ne0lith@users.noreply.github.com>
2023-12-12 15:21:17 -0600

    Update looper_mods.bat

--- looper_mods.bat
+++ looper_mods.bat
@@ -10,12 +10,9 @@ for /L %%i in (1, 1, %loop_count%) do (
     echo Running iteration %%i of %loop_count%
     call :RunSubBatch
 
-    REM Call Postrun functions
-    REM Backup all logs each run (this can take up GIGS in no time. be sure to delete every once in a while.)
     if "%backup_logs%"=="true" (
         python "mods\file_management.py" "!profile_name!"
     )
-    REM Send a discord webhook to notify you of final numbers
     if "%send_webhook%"=="true" (
         python "mods\send_webhook_results.py" "!profile_name!" "!machine_name!"
     )
