51dba3c386ab68bdcbc9e606d0c54790428014de
ne0lith <118079899+ne0lith@users.noreply.github.com>
2023-12-12 15:15:18 -0600

    Update looper.bat
    Add sleep between each loop

--- looper.bat
+++ looper.bat
@@ -9,6 +9,11 @@ for /f "delims=" %%a in (config.txt) do set %%a
 for /L %%i in (1, 1, %loop_count%) do (
     echo Running iteration %%i of %loop_count%
     call :RunSubBatch
+    echo Completed iteration %%i of %loop_count%
+    echo
+    echo Sleeping for !sleep_time! seconds before the next iteration...
+    timeout /t !sleep_time! /nobreak >nul
+
 )
 
 goto :eof
